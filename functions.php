9e3b920e1b024c2c3a66ea23fe7d54dbc1213d29
Matthias <tthias@hotmail.fr>
2024-10-10 11:39:58 +0200

    modification

--- functions.php
+++ functions.php
@@ -32,6 +32,75 @@ if (!function_exists('nathalie_mota_theme_setup')) {
         // JS
         wp_enqueue_script('theme-script', get_template_directory_uri() . '/assets/js/index.js', array('jquery'), '1.0', true);
 
-}
+        // script personnalisé pour la pagination Ajax
+        wp_enqueue_script('custom-ajax', get_template_directory_uri() . '/assets/js/custom-ajax.js', array('jquery'), '1.0', true);
+        wp_localize_script('custom-ajax', 'myAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
+
+        // script lightbox
+        wp_enqueue_script('lightbox', get_template_directory_uri() . '/assets/js/lightbox.js', array('jquery'), '1.0', true);
+
+        // Bibliothèque Font Awesome
+        wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', array());
+
+        // Bibliothèque Select2 pour les selects de tri
+        wp_enqueue_script('select2-js', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js', array('jquery'), '4.0.13', true);
+        wp_enqueue_style('select2-css', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css', array());
+    }
 
 add_action('wp_enqueue_scripts', 'theme_scripts');
+
+function filtre_load_photos_ajax()
+{
+    $paged = isset($_POST['page']) ? intval($_POST['page']) + 1 : 1;
+
+    // Vérifier et sécuriser les valeurs des filtres
+    $cat_filter = isset($_POST['champ_categorie']) ? sanitize_text_field($_POST['champ_categorie']) : '';
+    $format_filter = isset($_POST['champ_format']) ? sanitize_text_field($_POST['champ_format']) : '';
+
+    $args = array(
+        'post_type' => 'photos',
+        'posts_per_page' => 12,
+        'paged' => $paged,
+        'tax_query' => array(),
+        'meta_query' => array(),
+    );
+
+    if (!empty($cat_filter)) {
+        $args['tax_query'][] = array(
+            'taxonomy' => 'categorie',
+            'field' => 'slug',
+            'terms' => $cat_filter,
+        );
+    }
+
+    if (!empty($format_filter)) {
+        $args['tax_query'][] = array(
+            'taxonomy' => 'format',
+            'field' => 'slug',
+            'terms' => $format_filter,
+        );
+    }
+
+
+    $photos_query = new WP_Query($args);
+
+    ob_start();
+
+    if ($photos_query->have_posts()) {
+        while ($photos_query->have_posts()) {
+            $photos_query->the_post();
+            // Structure du catalogue
+            get_template_part('template-parts/content/container_photos');
+        }
+    }
+
+    wp_reset_postdata();
+
+    $response = ob_get_clean();
+    echo $response;
+    exit;
+}
+
+// Action WordPress pour la requête AJAX
+add_action('wp_ajax_filtre_load_photos_ajax', 'filtre_load_photos_ajax');
+add_action('wp_ajax_nopriv_filtre_load_photos_ajax', 'filtre_load_photos_ajax');
